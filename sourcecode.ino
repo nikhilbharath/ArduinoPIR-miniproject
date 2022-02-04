f7682b2cb2ed6234b7097a741a205c19dc00e103
Nikhil Bharath <nramasw3@binghamton.edu>
2022-02-04 17:34:14 -0500

    Add files via upload

---
+++ sourcecode.ino
@@ -0,0 +1,32 @@
+int led = 13;
+int sensor = 2;
+int state = LOW;
+int val = 0;
+
+void setup() {
+  pinMode(led, OUTPUT);      
+  pinMode(sensor, INPUT);
+  Serial.begin(9600);
+}
+
+void loop(){
+  val = digitalRead(sensor);
+  if (val == HIGH) {
+    digitalWrite(led, HIGH);
+    delay(100);
+
+    if (state == LOW) {
+      Serial.println("Motion detected!");
+      state = HIGH;
+    }
+  }
+  else {
+      digitalWrite(led, LOW);
+      delay(200);
+
+      if (state == HIGH){
+        Serial.println("Motion stopped!");
+        state = LOW;
+    }
+  }
+}
